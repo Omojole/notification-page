990e43fcf3b0fbf6cb4e84b36963615be769968f
Omojole Aishat <aishat.omojole@gmail.com>
2022-09-29 18:39:59 +0100

    Working on JS

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const notificationPopup = document.querySelectorAll(".notificationPopupClass");
+const notificationNoEl=document.querySelector('.notification-no');
 const icons = document.querySelectorAll(".fa-solid");
-notificationPopup.forEach((notificationPopupclass, i) => {
-  notificationPopupclass.onclick = function () {
-    const notificationNo = 1 + i;
-    document
-      .getElementById(`popup_${notificationNo}`)
-      .classList.remove("notificationPopupClass");
-    document
-      .getElementById(`popup_${notificationNo}`)
-      .classList.add("notification-color");
+let  notificationNo=7; 
 
-    document
-      .getElementById(`activeIcon_${notificationNo}`)
-      .classList.remove("fa-solid");
-    document
-      .getElementById(`activeIcon_${notificationNo}`)
-      .classList.remove("fa-circle");
-    console.log(notificationNo);
-  };
-});
+notificationPopup.forEach((notificationPopupclass, i) => 
+notificationPopupclass.onclick = function () {
+  const notificationClicked = 1 + i;
+  document
+  .getElementById(`popup_${notificationClicked}`)
+  .classList.remove("notificationPopupClass");
+  document
+  .getElementById(`popup_${notificationClicked}`)
+  .classList.add("notification-color");
+  
+  document
+  .getElementById(`activeIcon_${notificationClicked}`)
+  .classList.remove("fa-solid");
+  document
+  .getElementById(`activeIcon_${notificationClicked}`)
+  .classList.remove("fa-circle");
+  // if(document.querySelector('.notification-box').classList.contains(notificationPopupclass)){
+  //   notificationNo>=0;notificationNo--
+  //     console.log(notificationNo);
+  // notificationNoEl.textContent=notificationNo
+  // }
+  });
+// console.log(document.querySelectorAll('.notification-box').classList);
 
 //notification numbering,font,responsiveness
