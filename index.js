ea6f570149ca9057d3bc9401fb5f13ab3b6ab5e8
Omojole Aishat <aishat.omojole@gmail.com>
2022-09-30 09:48:33 +0100

    JS refactoring

--- index.js
+++ index.js
@@ -2,29 +2,23 @@ const notificationPopup = document.querySelectorAll(".notificationPopupClass");
 const notificationNoEl=document.querySelector('.notification-no');
 const icons = document.querySelectorAll(".fa-solid");
 let  notificationNo=7; 
-
-notificationPopup.forEach((notificationPopupclass, i) => 
-notificationPopupclass.onclick = function () {
-  const notificationClicked = 1 + i;
-  document
-  .getElementById(`popup_${notificationClicked}`)
-  .classList.remove("notificationPopupClass");
-  document
-  .getElementById(`popup_${notificationClicked}`)
-  .classList.add("notification-color");
-  
-  document
-  .getElementById(`activeIcon_${notificationClicked}`)
-  .classList.remove("fa-solid");
-  document
-  .getElementById(`activeIcon_${notificationClicked}`)
-  .classList.remove("fa-circle");
-  // if(document.querySelector('.notification-box').classList.contains(notificationPopupclass)){
-  //   notificationNo>=0;notificationNo--
-  //     console.log(notificationNo);
-  // notificationNoEl.textContent=notificationNo
-  // }
-  });
-// console.log(document.querySelectorAll('.notification-box').classList);
-
+let playing=true;
 //notification numbering,font,responsiveness
+  for(let i=0;i<notificationPopup.length;i++)
+  notificationPopup[i].addEventListener('click',function(){if(notificationNo>0){
+  const number=i+1;
+    document.getElementById(`popup_${number}`).classList.add('notification-color');
+    document.getElementById(`popup_${number}`).classList.remove('notificationPopupClass');
+      
+    document
+    .getElementById(`activeIcon_${number}`)
+    .classList.remove("fa-solid");
+    document
+    .getElementById(`activeIcon_${number}`)
+    .classList.remove("fa-circle");
+
+    notificationNo>=0;notificationNo-=1
+notificationNoEl.textContent=notificationNo
+  }
+    })
+    // console.log(document.querySelector('.notification-box').classList.contains('notificationPopupClass'));
