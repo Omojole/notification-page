e8a6f314c83c1d2ab20f17856356a9c4ad762cef
Omojole Aishat <aishat.omojole@gmail.com>
2022-10-02 22:09:04 +0100

    JS done

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
 'use strict'
-const notificationPopup = document.querySelectorAll(".notification-box");
+const notificationPopup =document.querySelectorAll(".notification-box");
 const notificationNoEl=document.querySelector('.notification-no');
 const icons = document.querySelectorAll(".fa-solid");
 let notificationNo=7;
-notificationPopup.forEach((element,index) => {
-	
-	element.addEventListener('click',function(){
 
-	        let number=index+1;
-	        document.getElementById(`popup_${number}`).classList.add('notification-color');
-	        document.getElementById(`popup_${number}`).classList.remove('notificationPopupClass');
+// notificationPopup.forEach((element,index) => {
+	
+// 	element.addEventListener('click',function(){
+
+// 	        let number=index+1;
+// 	        document.getElementById(`popup_${number}`).classList.add('notification-color');
+// 	        document.getElementById(`popup_${number}`).classList.remove('notificationPopupClass');
 	        
-	        document
-	        .getElementById(`activeIcon_${number}`)
-	        .classList.remove("fa-solid");
-	        document
-	        .getElementById(`activeIcon_${number}`)
-	        .classList.remove("fa-circle");
-          if(element.classList.contains('notification-colordone')){
-            return;
-          }
-          if(notificationNo>0){
-            document.getElementById(`popup_${number}`).classList.add('notification-color'+'done')
-            notificationNo--;
+// 	        document
+// 	        .getElementById(`activeIcon_${number}`)
+// 	        .classList.remove("fa-solid");
+// 	        document
+// 	        .getElementById(`activeIcon_${number}`)
+// 	        .classList.remove("fa-circle");
+//           if(element.classList.contains('notification-colordone')){
+//             return;
+//           }
+//           if(notificationNo>0){
+//             document.getElementById(`popup_${number}`).classList.add('notification-color'+'done')
+//             notificationNo--;
             
-            notificationNoEl.textContent=notificationNo
-          }
+//             notificationNoEl.textContent=notificationNo
+//           }
 	        
-	      })
-	    }
-	);
-	
+// 	      })
+// 	    }
+// 	);
 
+for(let i=0;i<notificationPopup.length;i++)
+notificationPopup[i].addEventListener('click',function(){if(notificationNo>0){
+   let number=i+1;
+      document.getElementById(`popup_${number}`).classList.add('notification-color');
+      document.getElementById(`popup_${number}`).classList.remove('notificationPopupClass');
+        
+      document
+      .getElementById(`activeIcon_${number}`)
+      .classList.remove("fa-solid");
+      document
+      .getElementById(`activeIcon_${number}`)
+      .classList.remove("fa-circle");
+    
+      notificationNo>=0;notificationNo-=1
+      notificationNoEl.textContent=notificationNo
+    }
+
+  },
+  {once:true})
   //Mark all as read
   document.querySelector('.read').addEventListener('click',function(){
     notificationNoEl.textContent=0
@@ -49,9 +68,3 @@ notificationPopup.forEach((element,index) => {
     }
     )
   })
-//   console.log(document.querySelectorAll('.nootification-box'));
-//   notification numbering,font,responsiveness
-// if(document.querySelector('.notification-box').classList.contains('notificationPopupClass')){
-//   action()
-// }
-//     console.log(document.getElementsByClassName('notification-box').classList.contains('notificationPopupClass'));
